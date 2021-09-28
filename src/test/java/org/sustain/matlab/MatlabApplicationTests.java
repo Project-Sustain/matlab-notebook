712db72a4b1480ab232fa4e9ff2112140f7708bb
Caleb Carlson <caleb.carlson@hpe.com>
2021-09-27 20:23:02 -0600

    Hard reset to Spring Boot Initializr project

---
+++ src/test/java/org/sustain/matlab/MatlabApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sustain.matlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
