cc2a2d50d20b343097fccd6ce6f1ba7a41e69e96
Caleb Carlson <caleb.carlson@hpe.com>
2021-11-11 14:16:16 -0700

    debugging

--- src/main/java/org/sustain/mongodb/MongoQuery.java
+++ src/main/java/org/sustain/mongodb/MongoQuery.java
@@ -54,11 +54,7 @@ public class MongoQuery {
                         Aggregates.match(Filters.eq("GISJOIN", "G1200870")),
                         Aggregates.group(
                                 null,
-                                Accumulators.max("max_date", "$YYYYMMDDHH")
-                        ),
-                        Aggregates.group(
-                                null,
-                                Accumulators.min("min_date", "$YYYYMMDDHH")
+                                Accumulators.max("$YYYYMMDDHH", "max_date")
                         )
                 )
         );
