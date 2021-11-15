5c4d846291d22c60b4051f8b40858f8d7dd691b4
Caleb Carlson <caleb.carlson@hpe.com>
2021-11-15 11:13:21 -0700

    Move MatlabApplication -> Application

--- src/main/java/org/sustain/Application.java
+++ src/main/java/org/sustain/Application.java
@@ -1,4 +1,4 @@
-package org.sustain.matlab;
+package org.sustain;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -6,16 +6,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MatlabApplication {
+public class Application {
 
 	private static final String VERSION = "0.1";
 
-	public static Logger log = LoggerFactory.getLogger(MatlabApplication.class);
+	public static Logger log = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
 		log.info("Starting MatlabApplication v{}", VERSION);
 
-		SpringApplication.run(MatlabApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
