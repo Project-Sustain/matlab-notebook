251df0d508c1a9d7ab1fa5165ea7af0e14682761
Caleb Carlson <caleb.carlson@hpe.com>
2021-11-15 10:52:40 -0700

    Add version hardcoded

--- src/main/java/org/sustain/matlab/MatlabApplication.java
+++ src/main/java/org/sustain/matlab/MatlabApplication.java
@@ -4,14 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.sustain.mongodb.MongoQuery;
 
 @SpringBootApplication
 public class MatlabApplication {
 
+	private static final String VERSION = "0.1";
+
 	public static Logger log = LoggerFactory.getLogger(MatlabApplication.class);
 
 	public static void main(String[] args) {
+		log.info("Starting MatlabApplication v{}", VERSION);
 		SpringApplication.run(MatlabApplication.class, args);
 	}
 
