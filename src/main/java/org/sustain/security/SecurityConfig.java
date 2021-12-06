8b1cc47061109ad23a638b276027a92b17959895
Caleb Carlson <caleb.carlson@hpe.com>
2021-12-06 14:06:33 -0700

    Remove securityconfig

--- src/main/java/org/sustain/security/SecurityConfig.java
+++ src/main/java/org/sustain/security/SecurityConfig.java
@@ -1,10 +1,5 @@
 package org.sustain.security;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
 // @Configuration
 public class SecurityConfig {
 
