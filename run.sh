abd90883014271643c27bfaacd7af7cda0873673
Sustain Account <sustain@cs.colostate.edu>
2022-03-10 09:37:41 -0700

    Create run script

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH="/usr/local/matlab/latest/bin/glnxa64:/usr/local/matlab/latest/sys/os/glnxa64" && ./gradlew clean && ./gradlew build && ./gradlew bootRun
-#export LD_LIBRARY_PATH="/usr/local/matlab/latest/bin/glnxa64:/usr/local/matlab/latest/sys/os/glnxa64" && ./gradlew clean build && java -cp build/libs/matlab-notebook-0.0.1-SNAPSHOT-uber.jar Main
+export LD_LIBRARY_PATH="/usr/local/matlab/latest/bin/glnxa64:/usr/local/matlab/latest/sys/os/glnxa64" && ./gradlew clean && ./gradlew build
+
+if [[ $? -eq 0 ]]; then
+  echo -e "Build successful, launching Spring Boot application on $(hostname):9001 as a daemon, logging to matlab-notebook.log"
+  nohup java -jar build/libs/matlab-notebook-0.0.1-SNAPSHOT.jar > matlab-notebook.log 2>&1 & disown
+fi
